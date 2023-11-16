7ba8843c5b4b4d735c309e3fd8e23a8f6060fe53
vidit jain <jainvidit365vidit@gmail.com>
2023-11-16 23:42:11 +0530

    adding dashboard button

---
+++ my-react-app/src/components/dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './dashboard.css';
+
+function dashboard(){
+    return (
+        <div>
+          <h1>This is My Component</h1>
+          <button >Click me</button>
+        </div>
+      );
+    }
+export  default dashboard;
