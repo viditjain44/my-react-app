d6649241e0899b3a22ff317eba3d0140f5510256
vidit jain <jainvidit365vidit@gmail.com>
2023-11-22 23:01:51 +0530

    making responsive

--- my-react-app/src/components/Dashinner.jsx
+++ my-react-app/src/components/Dashinner.jsx
@@ -9,8 +9,9 @@ function Dashinner(){
        <button className="crypto"> <b>Crypto</b></button>
        <h2 className="sensex">Sensex : <h2 className="svalue">64587.3<h1 className="profit">+22.15</h1></h2></h2>
        <h2 className="nifty">Nifty : <h2 className="svalue2">64587.3<h1 className="profit">-22.15</h1></h2> </h2>
-       <button className="bitcoin"></button>
+        <div className="button"><button className="bitcoin"></button>
        <button className="bitcoin"></button><button className="bitcoin"></button><button className="bitcoin"></button>
+       </div>
 
         <div>
         <button id="Stocks">Stocks</button>
